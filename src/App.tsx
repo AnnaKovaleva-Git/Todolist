e76ef8c70c640c03f7ea08a04d5296cf60bdf9ce
Kovalev <Kovalev.K@citilink.ru>
2023-05-23 11:49:39 +0300

    02

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import {Todolist} from "./Todolist";
 
 function App() {
+
+    const truck='What to learn1 fssgfd xdffffffffffffffffffffff'
+    const truckNew='What to learn222'
+
+    const tasks1 = [
+        { id: 1, title: "HTML&CSS", isDone: true }, //0
+        { id: 2, title: "JS", isDone: true }, //1
+        { id: 3, title: "ReactJS", isDone: false },
+        { id: 4, title: "ReactJS4444444", isDone: false },
+       ]
+    const tasks2 = [
+        { id: 1, title: "Hello world", isDone: true },
+        { id: 2, title: "I am Happy", isDone: false },
+        { id: 3, title: "Yo", isDone: false }
+    ]
+
+
+
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <Todolist truck={truck} truck2={100200} tasks={tasks1}/>
+            <Todolist truck={truckNew}  truck3={true} tasks={tasks2} />
         </div>
+
     );
 }
 
